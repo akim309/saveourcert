42eed682397e1e12875c12d8ea7d9e324eb19076
Ashley Kim <ashleykim@localhost.localdomain>
2020-07-27 14:18:42 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router, 
+  Route,
+} from 'react-router-dom';
 import './App.css';
+import RegistrationForm from './pages/RegistrationForm';
+import CertificationWall from './pages/CertificationWall';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div id="page-body">
+          <NavBar />
+          <Route path="/certificationWall" component={CertificationWall} exact />
+          <Route path="/register" component={RegistrationForm} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
